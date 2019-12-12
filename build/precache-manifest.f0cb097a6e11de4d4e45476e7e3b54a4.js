f3c1e69206146d644a7675d6689792558a9a5396
antuan159 <lanos159@ukr.net>
2019-12-12 10:17:12 +0200

    build

---
+++ build/precache-manifest.f0cb097a6e11de4d4e45476e7e3b54a4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2e5a6922e4b81df334662410319b9cc",
+    "url": "/goit-react-hw-04-movies/index.html"
+  },
+  {
+    "revision": "32c02c5bfcf619f5dc32",
+    "url": "/goit-react-hw-04-movies/static/css/main.eb634274.chunk.css"
+  },
+  {
+    "revision": "2f3018020503f2dbf0a8",
+    "url": "/goit-react-hw-04-movies/static/js/2.7d9fb396.chunk.js"
+  },
+  {
+    "revision": "32c02c5bfcf619f5dc32",
+    "url": "/goit-react-hw-04-movies/static/js/main.b203a3c4.chunk.js"
+  },
+  {
+    "revision": "01b129782775a6c19017",
+    "url": "/goit-react-hw-04-movies/static/js/runtime-main.49d45353.js"
+  }
+]);
